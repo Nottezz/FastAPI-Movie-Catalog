f641d87f8bb34fc2131dfbd475ff897c000e5563
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-04-02 11:29:25 +0300

    add new endpoint: add movie

--- movie-catalog/api/api_v1/movie_catalog/views.py
+++ movie-catalog/api/api_v1/movie_catalog/views.py
@@ -1,6 +1,8 @@
+import random
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from annotated_types import Len
+from fastapi import APIRouter, Depends, status, Form
 
 from schemas.movie_catalog import MovieCatalog
 from .crud import MOVIE_LIST
@@ -16,10 +18,32 @@ router = APIRouter(
     "/",
     response_model=list[MovieCatalog],
 )
-def get_films_list() -> list[MovieCatalog]:
+def get_movie_list() -> list[MovieCatalog]:
     return MOVIE_LIST
 
 
+@router.post(
+    "/",
+    response_model=MovieCatalog,
+    status_code=status.HTTP_201_CREATED,
+)
+def add_movie(
+    title: Annotated[str, Len(min_length=3, max_length=50), Form()],
+    description: Annotated[str, Len(min_length=20, max_length=500), Form()],
+    year_released: Annotated[int, Form()],
+    rating: Annotated[float, Form()],
+) -> MovieCatalog:
+    return MovieCatalog(
+        id=random.randint(1, 100),
+        title=title,
+        description=description,
+        year_released=year_released,
+        rating=rating,
+    )
+
+
 @router.get("/{movie_id}", response_model=MovieCatalog)
-def get_film(movie_id: Annotated[MovieCatalog, Depends(prefetch_film)]) -> MovieCatalog:
+def get_movie(
+    movie_id: Annotated[MovieCatalog, Depends(prefetch_film)]
+) -> MovieCatalog:
     return movie_id
