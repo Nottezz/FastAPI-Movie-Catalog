c025768b77ff2dc37ea060bc059782deadec7dea
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-04-02 12:46:14 +0300

    edit view for creating movie

--- movie-catalog/api/api_v1/movie_catalog/views.py
+++ movie-catalog/api/api_v1/movie_catalog/views.py
@@ -1,10 +1,9 @@
 import random
 from typing import Annotated
 
-from annotated_types import Len
-from fastapi import APIRouter, Depends, status, Form
+from fastapi import APIRouter, Depends, status
 
-from schemas.movie_catalog import MovieCatalog
+from schemas.movie_catalog import MovieCatalog, MovieCatalogCreate
 from .crud import MOVIE_LIST
 from .dependencies import prefetch_film
 
@@ -28,17 +27,11 @@ def get_movie_list() -> list[MovieCatalog]:
     status_code=status.HTTP_201_CREATED,
 )
 def add_movie(
-    title: Annotated[str, Len(min_length=3, max_length=50), Form()],
-    description: Annotated[str, Len(min_length=20, max_length=500), Form()],
-    year_released: Annotated[int, Form()],
-    rating: Annotated[float, Form()],
+    movie_create: MovieCatalogCreate,
 ) -> MovieCatalog:
     return MovieCatalog(
         id=random.randint(1, 100),
-        title=title,
-        description=description,
-        year_released=year_released,
-        rating=rating,
+        **movie_create.model_dump(),
     )
 
 
