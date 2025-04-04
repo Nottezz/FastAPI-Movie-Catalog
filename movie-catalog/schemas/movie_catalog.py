9d3b2210e24de0241f9621b7bf76a3a4010f30c4
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-04-04 22:10:56 +0300

    add schema for update movie

--- movie-catalog/schemas/movie_catalog.py
+++ movie-catalog/schemas/movie_catalog.py
@@ -45,9 +45,40 @@ class MovieCreate(MovieBase):
     )
 
 
+class MovieUpdate(MovieBase):
+    """
+    Модель для обновления фильма
+    """
+
+    title: str = Field(
+        ...,
+        min_length=3,
+        max_length=50,
+        title="Movie title",
+    )
+    description: str = Field(
+        ...,
+        title="Movie description",
+        min_length=20,
+        max_length=500,
+    )
+    year_released: int = Field(
+        1900,
+        ge=0,
+        le=9999,
+        title="Year released",
+    )
+    rating: float = Field(
+        1.0,
+        ge=0.0,
+        le=10.0,
+        title="Movie rating",
+    )
+
+
 class Movie(MovieBase):
     """
-    Модель каталога фильмов
+    Модель фильма
     """
 
     slug: str
