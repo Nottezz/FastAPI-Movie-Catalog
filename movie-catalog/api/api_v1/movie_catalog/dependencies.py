92558120bba231d0ac1bdb31811a759f41eb73e3
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-04-04 20:33:10 +0300

    fix error message

--- movie-catalog/api/api_v1/movie_catalog/dependencies.py
+++ movie-catalog/api/api_v1/movie_catalog/dependencies.py
@@ -8,5 +8,6 @@ def prefetch_film(movie_slug: str) -> Movie:
     films: Movie | None = storage.get_by_slug(movie_slug)
     if films:
         return films
-
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Films not found")
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail=f"Movie {movie_slug!r} not found"
+    )
