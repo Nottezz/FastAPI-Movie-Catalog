865b8e3daa1902a7d2eccf9445147c1daa794005
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-04-07 23:44:54 +0300

    add to config log level and format

--- movie-catalog/config.py
+++ movie-catalog/config.py
@@ -1,4 +1,7 @@
+import logging
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 STORAGE_PATH = BASE_DIR / "movie-catalog.json"
+LOG_FORMAT = "[%(levelname)s] (%(asctime)s) %(module)s-%(lineno)d: %(message)s"
+LOG_LEVEL = logging.INFO
