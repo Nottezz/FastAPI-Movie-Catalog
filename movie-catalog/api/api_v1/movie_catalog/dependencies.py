5be5aac8daffee912467669c44fa2f0464fdefc3
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-04-09 21:28:57 +0300

    add dependence for apply background task

--- movie-catalog/api/api_v1/movie_catalog/dependencies.py
+++ movie-catalog/api/api_v1/movie_catalog/dependencies.py
@@ -1,7 +1,11 @@
-from fastapi import HTTPException, status
+import logging
+
+from fastapi import HTTPException, status, BackgroundTasks
 
 from schemas.movie_catalog import Movie
 from .crud import storage
+
+logger = logging.getLogger(__name__)
 
 
 def prefetch_film(movie_slug: str) -> Movie:
@@ -11,3 +15,9 @@ def prefetch_film(movie_slug: str) -> Movie:
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND, detail=f"Movie {movie_slug!r} not found"
     )
+
+
+def save_storage_state(background_tasks: BackgroundTasks):
+    yield
+    logger.debug("Add background tasks to save storage")
+    background_tasks.add_task(storage.save_storage)
