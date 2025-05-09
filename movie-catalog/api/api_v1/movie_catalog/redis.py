d8dcd18cf4a390144074020b88904c4591b160a3
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-05-09 15:43:05 +0300

    add redis token helper

--- movie-catalog/api/api_v1/movie_catalog/redis.py
+++ movie-catalog/api/api_v1/movie_catalog/redis.py
@@ -1,7 +1,55 @@
+import secrets
+from abc import ABC, abstractmethod
+
 from redis import Redis
 
-from config import REDIS_HOST, REDIS_PORT, REDIS_DB_TOKENS
+from config import REDIS_HOST, REDIS_PORT, REDIS_DB_TOKENS, REDIS_TOKENS_SET_NAME
 
-redis_tokens = Redis(
-    host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB_TOKENS, decode_responses=True
+
+class AbstractTokensHelper(ABC):
+
+    @abstractmethod
+    def token_exists(self, token: str) -> bool:
+        """
+        Check if token exists.
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def add_token(self, token: str) -> None:
+        """
+        Save token to storage.
+        :param token:
+        :return:
+        """
+        pass
+
+    @classmethod
+    def generate_token(cls) -> str:
+        return secrets.token_urlsafe(16)
+
+    def generate_and_save_token(self) -> str:
+        token = self.generate_token()
+        self.add_token(token)
+        return token
+
+
+class RedisTokensHelper(AbstractTokensHelper):
+    def __init__(self, host: str, port: int, db: int, tokens_set_name: str) -> None:
+        self.redis = Redis(host=host, port=port, db=db, decode_responses=True)
+        self.tokens_set = tokens_set_name
+
+    def token_exists(self, token: str) -> bool:
+        return bool(self.redis.sismember(self.tokens_set, token))
+
+    def add_token(self, token: str) -> None:
+        self.redis.sadd(self.tokens_set, token)
+
+
+redis_tokens = RedisTokensHelper(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    db=REDIS_DB_TOKENS,
+    tokens_set_name=REDIS_TOKENS_SET_NAME,
 )
