1559562ac2eee4dfd7e98d89ad3ef1a3119a7e47
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-05-16 08:19:46 +0300

    add typer with commands

---
+++ movie-catalog/commands/cli.py
@@ -0,0 +1,15 @@
+__all__ = "app"
+
+import typer
+from .hello import app as hello_app
+
+app = typer.Typer(no_args_is_help=True, rich_markup_mode="rich")
+
+app.add_typer(hello_app)
+
+
+@app.callback()
+def callback() -> None:
+    """
+    Some CLI management commands
+    """
