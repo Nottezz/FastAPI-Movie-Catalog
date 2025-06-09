60c378104eca9a94d2de3ae7c52b6c4b8b6afbde
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-06-09 17:38:08 +0300

    tests for two schemas: Update and Partial Update movie

--- movie-catalog/tests/test_schemas/test_movie_catalog.py
+++ movie-catalog/tests/test_schemas/test_movie_catalog.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from schemas.movie_catalog import MovieCreate, Movie
+from schemas.movie_catalog import MovieCreate, MovieUpdate, MoviePartialUpdate, Movie
 
 
 class MovieCreateTestCase(TestCase):
@@ -20,3 +20,53 @@ class MovieCreateTestCase(TestCase):
         self.assertEqual(movie_in.description, movie.description)
         self.assertEqual(movie_in.year_released, movie.year_released)
         self.assertEqual(movie_in.rating, movie.rating)
+
+
+class MovieUpdateTestCase(TestCase):
+    def test_movie_update_can_be_updated_from_update_schema(self) -> None:
+        movie = Movie(
+            slug="some-slug",
+            title="Some title",
+            description="Some description for unit-test",
+            year_released=1901,
+            rating=1.0,
+        )
+
+        movie_update = MovieUpdate(
+            title="Some title from update schema",
+            description="Some description for unit-test from update schema",
+            year_released=2001,
+            rating=10.0,
+        )
+
+        for field, value in movie_update:
+            setattr(movie, field, value)
+
+        self.assertEqual(movie_update.title, movie.title)
+        self.assertEqual(movie_update.description, movie.description)
+        self.assertEqual(movie_update.year_released, movie.year_released)
+        self.assertEqual(movie_update.rating, movie.rating)
+
+
+class MoviePartialUpdateTestCase(TestCase):
+    def test_movie_partial_update_can_be_updated_from_partial_update_schema(
+        self,
+    ) -> None:
+        movie = Movie(
+            slug="some-slug",
+            title="Some title",
+            description="Some description for unit-test",
+            year_released=1901,
+            rating=1.0,
+        )
+
+        movie_partial_update = MoviePartialUpdate(
+            title="Some title from partial update schema",
+            year_released=2001,
+        )
+
+        for field, value in movie_partial_update.model_dump(exclude_unset=True).items():
+            setattr(movie, field, value)
+
+        self.assertEqual(movie_partial_update.title, movie.title)
+        self.assertEqual(movie_partial_update.year_released, movie.year_released)
