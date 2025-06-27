75bb9e9653b76d05a195e421d43ffc29fca58130
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-06-27 18:59:33 +0300

    test for root view

--- movie_catalog/tests/conftest.py
+++ movie_catalog/tests/conftest.py
@@ -1,3 +1,4 @@
+import logging
 from os import getenv
 
 import pytest
@@ -6,3 +7,7 @@ if getenv("TESTING") != "1":
     pytest.exit(
         "Environment is not ready for testing",
     )
+
+@pytest.fixture(autouse=True)
+def disable_logging():
+    logging.getLogger().setLevel(logging.CRITICAL)
