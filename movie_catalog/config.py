01fa38cd9fff1da396ba467a652fe2e80ec09231
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-07-24 17:55:53 +0300

    config to connect to REDIS_HOST from env

--- movie_catalog/config.py
+++ movie_catalog/config.py
@@ -7,7 +7,7 @@ STORAGE_PATH = BASE_DIR / "movie_catalog.json"
 LOG_FORMAT = "[-] %(asctime)s [%(levelname)s] %(module)s-%(lineno)d - %(message)s"
 LOG_LEVEL = logging.DEBUG
 
-REDIS_HOST = "localhost"
+REDIS_HOST = getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(getenv("REDIS_PORT", 0)) or 6379
 REDIS_DB = 0
 REDIS_DB_TOKENS = 1
