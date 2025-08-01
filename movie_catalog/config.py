b7369b579d7db22580830063a6f591e466bed61c
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-08-01 13:44:50 +0300

    add env.template

--- movie_catalog/config.py
+++ movie_catalog/config.py
@@ -53,7 +53,10 @@ class RedisConfig(BaseModel):
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         case_sensitive=False,
-        env_file=BASE_DIR / ".env",
+        env_file=(
+            BASE_DIR / ".env.template",
+            BASE_DIR / ".env",
+        ),
         env_prefix="MOVIE_CATALOG__",
         env_nested_delimiter="__",
     )
