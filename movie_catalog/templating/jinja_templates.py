1723cdd69ac7243a8a1e2121cac5abadd7264faa
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-09-27 18:25:59 +0300

    add context inject

--- movie_catalog/templating/jinja_templates.py
+++ movie_catalog/templating/jinja_templates.py
@@ -1,6 +1,19 @@
+from datetime import date
+
 from config import BASE_DIR
+from fastapi import Request
 from fastapi.templating import Jinja2Templates
+
+
+def inject_current_date(
+    request: Request,  # noqa: ARG001 Unused function argument: `request`
+) -> dict[str, date]:
+    return {"today": date.today()}
+
 
 templates = Jinja2Templates(
     directory=BASE_DIR / "templates",
+    context_processors=[
+        inject_current_date,
+    ],
 )
