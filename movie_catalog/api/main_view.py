59800f5c1ad8e5b079c7026a031c3084e87384f9
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-09-27 18:54:37 +0300

    add features as context

--- movie_catalog/api/main_view.py
+++ movie_catalog/api/main_view.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from templating.jinja_templates import templates
@@ -9,7 +11,19 @@ router = APIRouter()
 def home_page(
     request: Request,
 ) -> HTMLResponse:
-    return templates.TemplateResponse(request=request, name="home.html")
+
+    context: dict[str, Any] = {}
+    features = {
+        "🚀 Fast API": "High performance with automatic OpenAPI documentation generation.",
+        "⭐ Movie Ratings": "Rate movies and build your own catalog of the best ones.",
+        "️🛠️ Easy Integration": "Clean and simple codebase that is easy to extend and improve.",
+    }
+    context.update(
+        features=features,
+    )
+    return templates.TemplateResponse(
+        request=request, name="home.html", context=context
+    )
 
 
 @router.get("/about", include_in_schema=False, name="about")
