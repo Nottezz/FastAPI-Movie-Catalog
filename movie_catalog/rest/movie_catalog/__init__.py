9b9b36805cffafaa3871b0180994689a320b08a3
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-10-05 21:48:57 +0300

    add create view with full form validation

--- movie_catalog/rest/movie_catalog/__init__.py
+++ movie_catalog/rest/movie_catalog/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
+from .create_view import router as create_router
 from .list_view import router as list_router
 
 router = APIRouter(prefix="/movie-catalog", tags=["movie-catalog"])
 router.include_router(list_router)
+router.include_router(create_router)
---
+++ movie_catalog/rest/movie_catalog/create_view.py
@@ -0,0 +1,80 @@
+from typing import Any, Mapping
+
+from dependencies.movie_catalog import GetMovieCatalogStorage
+from exceptions import MovieAlreadyExists
+from fastapi import APIRouter, Request, status
+from fastapi.responses import HTMLResponse, RedirectResponse
+from pydantic import BaseModel, ValidationError
+from schemas.movie_catalog import MovieCreate
+from templating import templates
+
+router = APIRouter(
+    prefix="/create",
+)
+
+
+@router.get("/", name="movie-catalog:create-view")
+def get_page_add_movie_to_catalog(request: Request) -> HTMLResponse:
+    context: dict[str, Any] = {}
+    model_schema = MovieCreate.model_json_schema()
+    context.update(model_schema=model_schema)
+    return templates.TemplateResponse(
+        request=request,
+        name="movie-catalog/create.html",
+        context=context,
+    )
+
+
+def format_pydantic_errors(error: ValidationError) -> dict[str, str]:
+    return {err["loc"][0]: err["msg"] for err in error.errors()}
+
+
+def create_view_validation_response(
+    request: Request,
+    form_data: BaseModel | Mapping[str, Any] | None = None,
+    errors: dict[str, str] | None = None,
+    form_validated: bool = True,
+) -> HTMLResponse:
+    context: dict[str, Any] = {}
+    model_schema = MovieCreate.model_json_schema()
+    context.update(
+        model_schema=model_schema,
+        form_data=form_data,
+        errors=errors,
+        form_validated=form_validated,
+    )
+    return templates.TemplateResponse(
+        request=request,
+        name="movie-catalog/create.html",
+        context=context,
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+    )
+
+
+@router.post("/", name="movie-catalog:create", response_model=None)
+async def add_movie(
+    request: Request, storage: GetMovieCatalogStorage
+) -> RedirectResponse | HTMLResponse:
+    async with request.form() as form:
+        try:
+            movie_create = MovieCreate.model_validate(form)
+        except ValidationError as e:
+            errors = format_pydantic_errors(e)
+            return create_view_validation_response(
+                request=request, errors=errors, form_data=form
+            )
+
+    try:
+        storage.create_or_rise_if_exists(movie_create)
+    except MovieAlreadyExists:
+        errors = {
+            "slug": f"Movie with slug '{movie_create.slug}' already exists.",
+        }
+    else:
+        return RedirectResponse(
+            url=request.url_for("movie-catalog:list"),
+            status_code=status.HTTP_303_SEE_OTHER,
+        )
+    return create_view_validation_response(
+        errors=errors, request=request, form_data=movie_create
+    )
