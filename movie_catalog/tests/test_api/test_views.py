212b2865874dc73d148d5b3675bcd323f904d46b
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-10-05 19:29:01 +0300

    editing the test to get an HTML response

--- movie_catalog/tests/test_api/test_views.py
+++ movie_catalog/tests/test_api/test_views.py
@@ -1,12 +1,20 @@
 import pytest
+from fastapi import status
 from fastapi.testclient import TestClient
 
 
-@pytest.mark.apitest
-def test_root_view(client: TestClient) -> None:
+@pytest.mark.templatetest
+def test_root(client: TestClient) -> None:
     response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {
-        "message": "Hello World",
-        "docs_url": "http://testserver/docs",
-    }
+    assert response.status_code == status.HTTP_200_OK, response.text
+    assert response.template.name == "home.html"  # type: ignore[attr-defined]
+    assert "features" in response.context, response.context  # type: ignore[attr-defined]
+    assert isinstance(response.context["features"], dict)  # type: ignore[attr-defined]
+
+
+@pytest.mark.templatetest
+def test_about(client: TestClient) -> None:
+    response = client.get("/about")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.template.name == "about.html"  # type: ignore[attr-defined]
+    assert "today" in response.context, response.context  # type: ignore[attr-defined]
