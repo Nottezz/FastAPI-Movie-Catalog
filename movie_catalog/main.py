d4a86223e91d23243ab0b04d228085995e6edb79
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-10-05 19:43:48 +0300

    move HTML view to rest module

--- movie_catalog/main.py
+++ movie_catalog/main.py
@@ -1,9 +1,9 @@
 import logging
 
 from fastapi import FastAPI
+from rest import router as rest_router
 
 from movie_catalog.api import router as api_router
-from movie_catalog.api.main_view import router as main_router
 from movie_catalog.app_lifespan import lifespan
 from movie_catalog.config import settings
 
@@ -19,5 +19,5 @@ app = FastAPI(
     version="1.0",
     lifespan=lifespan,
 )
-app.include_router(main_router)
+app.include_router(rest_router)
 app.include_router(api_router)
