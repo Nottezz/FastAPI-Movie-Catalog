986fd6bbae509444294c75b621804da53a4a91a3
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-10-05 22:22:36 +0300

    add global dep for auth

---
+++ movie_catalog/dependencies/auth.py
@@ -0,0 +1,51 @@
+import logging
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from services.auth import redis_users
+from starlette.requests import Request
+
+logger = logging.getLogger(__name__)
+
+
+UNSAFE_METHOD = frozenset(
+    {
+        "POST",
+        "PUT",
+        "PATCH",
+        "DELETE",
+    }
+)
+user_basic_auth = HTTPBasic(
+    scheme_name="Basic auth",
+    description="Basic username + password auth",
+    auto_error=False,
+)
+
+
+def validate_basic_auth(credentials: HTTPBasicCredentials | None) -> None:
+    logger.debug("User credentials: %s", credentials)
+
+    if credentials and redis_users.validate_user_password(
+        username=credentials.username, password=credentials.password
+    ):
+        return
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid username or password.",
+        headers={"WWW-Authenticate": "Basic"},
+    )
+
+
+def user_basic_auth_required_for_unsafe_methods(
+    request: Request,
+    credentials: Annotated[
+        HTTPBasicCredentials | None, Depends(user_basic_auth)
+    ] = None,
+) -> None:
+    if request.method not in UNSAFE_METHOD:
+        return
+
+    validate_basic_auth(credentials=credentials)
